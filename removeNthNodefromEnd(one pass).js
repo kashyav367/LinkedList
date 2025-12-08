4aee73eac12d83005adcad6301b95484e501d2ad
Ankit kumar Singh <ankit12030621028@gmail.com>
2025-12-09 02:06:55 +0530

    12th commit

---
+++ removeNthNodefromEnd(one pass).js
@@ -0,0 +1,42 @@
+function ListNode(val,next = null){
+    this.val = val;
+    this.next = next;
+}
+
+function removeNthNodeFromEnd(head,n){
+    let sentinel = new ListNode();
+    sentinel.next = head;
+
+    let first = sentinel;
+    for(let i=0 ; i<n ; i++){
+        first = first.next;
+    }
+
+    let second = sentinel;
+    while(first.next){
+        second = second.next;
+        first = first.next;
+    }
+    second.next = second.next.next;
+    return sentinel.next;
+}
+
+function printList(head){
+     let output = "";
+     let curr = head;
+
+     while(curr){
+        output += curr.val + " -> ";
+        curr = curr.next;
+     }
+     console.log(output + null);
+}
+
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+let newHead = removeNthNodeFromEnd(head,2);
+printList(head);
